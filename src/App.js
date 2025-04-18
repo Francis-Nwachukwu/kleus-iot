054d85a9b3688a3be8b8c0680b5599ff84fe9a03
Francis Nwachukwu <francisnwachukwu100@gmail.com>
2025-04-18 11:41:48 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import Routing from "routes";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import { BrowserRouter as Router } from "react-router-dom";
-import { useWss } from "hooks/useWebsocket";
-import { useEffect } from "react";
+// import { useWss } from "hooks/useWebsocket";
+// import { useEffect } from "react";
 
 function App() {
-  const { ready, send } = useWss("ws://54.81.132.129:7788");
+  // const { ready, send } = useWss("ws://54.81.132.129:7788");
 
-  useEffect(() => {
-    if (ready) {
-      send("test message from frontend");
-    }
-  }, [ready, send]);
+  // useEffect(() => {
+  //   if (ready) {
+  //     send("test message from frontend");
+  //   }
+  // }, [ready, send]);
 
   return (
     <Router>
